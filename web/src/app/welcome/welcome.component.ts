303fb50a49744330c5ddd3835f349eeffa5278ee
ChoeChangwon <qwefgh90@naver.com>
2019-10-20 22:03:10 +0900

    coordinate the layout for mobile

--- web/src/app/welcome/welcome.component.ts
+++ web/src/app/welcome/welcome.component.ts
@@ -8,9 +8,15 @@ import { OAuthService } from '../oauth/service/o-auth.service';
 })
 export class WelcomeComponent implements OnInit {
 
+  showFeature = true;
+  
   constructor(public oauth: OAuthService) { }
 
   ngOnInit() {
+  }
+
+  toggle(){
+    this.showFeature = !this.showFeature;
   }
 
 }
